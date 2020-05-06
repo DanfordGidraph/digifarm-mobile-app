d06ad850b557a0f323b28a9a91646c52f62f2ea0
Dunford Mainor <danmainor@outlook.com>
2020-05-06 13:43:35 +0300

    ADD: UI Dsigns implementation for Sign In page

--- src/App/Scenes/TabScenes/AuthScenes/SignIn/index.js
+++ src/App/Scenes/TabScenes/AuthScenes/SignIn/index.js
@@ -1,21 +1,29 @@
 import React from 'react'
-import { StatusBar } from 'react-native'
+import { ImageBackground, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
-import firestore from '@react-native-firebase/firestore'
 import { bindActionCreators } from 'redux'
 import * as ActionCreators from '@actions'
 import * as ActionTypes from '@actions/ActionTypes'
+import firestore from '@react-native-firebase/firestore'
 import {
-  Container, Text, Content, View, Item, Input, Icon, Button,
+  Container, Text, Content, View, Item, Input, Icon, Button, Card, Form,
 } from 'native-base'
+import TextInputMask from 'react-native-text-input-mask'
 import { NotificationBell } from '@components'
-import { Utils, Colors, Constants } from '@common'
+import {
+  Utils, Colors, Constants, Images,
+} from '@common'
 import AwesomeAlert from 'react-native-awesome-alerts'
 
+import reactotron from 'reactotron-react-native'
 import styles from './styles'
 
 class SignIn extends React.Component {
     state = {
+      user_phone: '',
+      email_address: '',
+      password: '',
+      password_visible: false,
       showAlert: false,
       alert: {
         title: '', message: '', confirmText: 'Close', showLoading: false,
@@ -23,27 +31,74 @@ class SignIn extends React.Component {
     }
 
     render = () => {
-      const { showAlert, alert } = this.state
+      const {
+        user_phone, email_address, password, password_visible, showAlert, alert,
+      } = this.state
+      const { navigation } = this.props
       return (
         <Container style={styles().container}>
-            <StatusBar barStyle="light-content" backgroundColor={Colors.SafaricomGreen} />
-            <Content>
-                <Text style={styles().txt_tab_name}>SignIn</Text>
+          <ImageBackground style={styles().img_bg} source={Images.PrimaryBg} resizeMode="cover" resizeMethod='auto' >
+            <Content contentContainerStyle={styles().content} >
+
+              <View style={styles().view_close}>
+                <TouchableOpacity style={styles().touchable_close} onPress={() => navigation.goBack()}>
+                  <Icon style={styles().icon_close} name='window-close' type='MaterialCommunityIcons'/>
+                </TouchableOpacity>
+                <Text style={styles().txt_login}>Log In</Text>
+              </View>
+
+
+              <Card style={styles().card_signin}>
+                <Form style={styles().form} >
+
+                  <Text style={styles().txt_input_label} >Phone Number</Text>
+                  <Item regular style={styles().item_input} success={Utils.validateKenyanPhoneNumber(user_phone) === true} error={user_phone.length > 9 && !Utils.validateKenyanPhoneNumber(user_phone)} >
+                    <TextInputMask style={styles().input_phone} mask={'+254[000] [000] [000]'} keyboardType="phone-pad" placeholder={'+2547XX XXX XXX'} onChangeText={(formatted, extracted) => this.setState({ user_phone: extracted })} />
+                    <Icon style={styles().icon_success} name='phone-outline' type='MaterialCommunityIcons'/>
+                  </Item>
+
+                  <Text style={styles().txt_input_label} >Email Address (Optional)</Text>
+                  <Item regular style={styles().item_input} success={Utils.validateEmailAddress(email_address) === true} error={email_address.length > 3 && !Utils.validateEmailAddress(email_address)} >
+                    <Input keyboardType={'email-address'} onChangeText={(text) => this.setState({ email_address: text })} />
+                    <Icon style={styles().icon_success} name='email-outline' type='MaterialCommunityIcons'/>
+                  </Item>
+
+                  <Text style={styles().txt_input_label} >Password</Text>
+                  <Item regular style={styles().item_input} success={Utils.validatePasswordStrength(password) === true} error={password.length > 8 && !Utils.validatePasswordStrength(password)} >
+                    <Input keyboardType={'default'} secureTextEntry={!password_visible} onChangeText={(text) => this.setState({ password: text })} />
+                    <Icon onPress={() => this.setState({ password_visible: !password_visible })} style={styles().icon_success} name={password_visible ? 'eye-outline' : 'eye-off-outline'} type='MaterialCommunityIcons'/>
+                  </Item>
+
+                </Form>
+
+                <Button block success onPress={this.Login} style={styles().btn_login}>
+                  <Text style={styles().txt_btn} uppercase={false} >Log In</Text>
+                </Button>
+
+                <Text onPress={this.ForgotPassword} style={styles().txt_forgot_pass} >Forgot Password ?</Text>
+
+                <Text style={styles().txt_no_account} >Dont Have an Account ?
+                  <Text onPress={this.StartSignUp} style={styles().txt_forgot_pass} > Sign Up</Text>
+                </Text>
+              </Card>
+
+
             </Content>
-            <AwesomeAlert
-              show={showAlert}
-              showProgress={alert.showLoading}
-              title={alert.title}
-              message={alert.message}
-              closeOnTouchOutside
-              closeOnHardwareBackPress
-              showConfirmButton={!alert.showLoading}
-              confirmText={alert.confirmText}
-              confirmButtonColor={Colors.SafaricomGreen}
-              onConfirmPressed={this.hideAlert}
-              closeOnTouchOutside={false}
-              closeOnHardwareBackPress={false}
-            />
+          </ImageBackground>
+          <AwesomeAlert
+            show={showAlert}
+            showProgress={alert.showLoading}
+            title={alert.title}
+            message={alert.message}
+            closeOnTouchOutside
+            closeOnHardwareBackPress
+            showConfirmButton={!alert.showLoading}
+            confirmText={alert.confirmText}
+            confirmButtonColor={Colors.SafaricomGreen}
+            onConfirmPressed={this.hideAlert}
+            closeOnTouchOutside={false}
+            closeOnHardwareBackPress={false}
+          />
         </Container>
       )
     }
@@ -73,6 +128,12 @@ class SignIn extends React.Component {
       const unread_notifications_count = unread_notifications ? Object.keys(unread_notifications)?.length : 0
       navigation.setOptions({ headerRight: () => (<NotificationBell count={unread_notifications_count} navigation={navigation} />) })
     }
+
+    Login = async () => reactotron.log('Login Clicked')
+
+    ForgotPassword = async () => reactotron.log('ForgotPassword Clicked')
+
+    StartSignUp = async () => this.props.navigation.navigate(Constants.Scenes.SignupOrgDetails)
 
     UNSAFE_componentWillReceiveProps = (nextProps) => {
 
--- src/App/Scenes/TabScenes/AuthScenes/SignIn/styles.js
+++ src/App/Scenes/TabScenes/AuthScenes/SignIn/styles.js
@@ -5,15 +5,89 @@ import { Constants, Colors, Utils } from '@common'
 const { width, height } = Constants
 
 const styles = (props) => StyleSheet.create({
+  btn_login: {
+    borderRadius: 10,
+    height: height * 0.08,
+    marginVertical: height * 0.03,
+  },
+  card_signin: {
+    alignSelf: 'center',
+    borderRadius: 10,
+    height: height * 0.55,
+    paddingHorizontal: width * 0.08,
+    paddingVertical: height * 0.02,
+    width: width * 0.95,
+  },
   container: {
     backgroundColor: Colors.BackgroundColor,
     flex: 1,
-    paddingVertical: height * 0.02,
   },
-  txt_tab_name: {
+  content: {
+    backgroundColor: Colors.Transparent,
+    flex: 1,
+    justifyContent: 'space-between',
+    paddingBottom: height * 0.1,
+    paddingTop: height * 0.03,
+  },
+  icon_close: {
+    color: Colors.White,
+    fontSize: Utils.scaledSize(30),
+  },
+  img_bg: {
+    flex: 1,
+    height,
+    width,
+  },
+  input_phone: {
+    alignSelf: 'center',
+    fontSize: Utils.scaledSize(17),
+    fontWeight: '200',
+    height: height * 0.065,
+    paddingLeft: width * 0.025,
+    width: width * 0.69,
+  },
+  item_input: {
+    alignSelf: 'center',
+    borderRadius: 10,
+    height: height * 0.07,
+  },
+  touchable_close: {
+    justifyContent: 'center',
+  },
+  txt_btn: {
+    fontSize: Utils.scaledSize(20),
+  },
+  txt_forgot_pass: {
     alignSelf: 'center',
     color: Colors.SafaricomGreen,
-    fontSize: Utils.scaledSize(17),
+    fontSize: Utils.scaledSize(15),
+    fontWeight: '500',
+  },
+  txt_input_label: {
+    color: Colors.LightGray,
+    fontSize: Utils.scaledSize(12),
+    fontWeight: '200',
+    paddingLeft: width * 0.03,
+    paddingTop: height * 0.01,
+  },
+  txt_login: {
+    alignSelf: 'center',
+    color: Colors.White,
+    fontSize: Utils.scaledSize(23),
+  },
+  txt_no_account: {
+    alignSelf: 'center',
+    color: Colors.LightGray,
+    fontSize: Utils.scaledSize(15),
+    fontWeight: '500',
+    marginVertical: height * 0.01,
+  },
+  view_close: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: width * 0.05,
+    width: width * 0.6,
   },
 })
 
