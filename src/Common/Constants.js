35939d9de8b908e5ac700cd36a6c2ac69f6d9bc4
Dunford Mainor <danmainor@outlook.com>
2020-05-06 13:37:38 +0300

    ADD: Scene constants for Categories, Cart, Auth & More Scenes

--- src/Common/Constants.js
+++ src/Common/Constants.js
@@ -8,8 +8,23 @@ const Constants = {
     Tabs: 'Tabs',
   },
   Scenes: {
+    // Intro Scenes
     Splash: 'Splash',
+    // Home Scenes
     Home: 'Home',
+    // Category Scenes
+    Categories: 'Categories',
+    // Cart Scenes
+    Cart: 'Cart',
+    // Auth Scenes
+    Account: 'Account',
+    SignIn: 'SignIn',
+    SignupOrgDetails: 'SignupOrgDetails',
+    SignupOrgDocs: 'SignupOrgDocs',
+    SignupOrgContact: 'SignupOrgContact',
+    ForgotPassword: 'ForgotPassword',
+    // More Scenes
+    More: 'More',
   },
 }
 export default Constants
