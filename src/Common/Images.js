7cf634647aa3176128d11798dd916310605a897a
Dunford Mainor <danmainor@outlook.com>
2020-05-06 13:35:26 +0300

    ADD: Image assets app_logo_light, primary_bg & upload_preloader refs

--- src/Common/Images.js
+++ src/Common/Images.js
@@ -1,6 +1,9 @@
 const Images = {
   AppLogo: require('../Assets/app_logo.png'),
+  AppLogoLight: require('../Assets/app_logo_light.png'),
   SafLogo: require('../Assets/safaricom_logo.png'),
   SafLogoWhite: require('../Assets/safaricom_logo_white.png'),
+  PrimaryBg: require('../Assets/primary_bg.jpg'),
+  Uploading: require('../Assets/upload_preloader.gif'),
 }
 export default Images
